7b248b8436c7ee06097ef109ba3b57680db861d1
Luke Schlangen <lukeschlangen@gmail.com>
2017-10-24 19:36:42 -0500

    add comments for client side

--- countries/src/index.js
+++ countries/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// finds the element on the dom where id="root" and replaces the content with the export of App.js
+// this is similar to ng-view or ui-view in angularjs
 ReactDOM.render(
   <App />,
   document.getElementById('root')
